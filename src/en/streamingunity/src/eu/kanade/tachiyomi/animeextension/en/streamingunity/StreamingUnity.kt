202953093a77fc36ae7a67dd69797840d4d61d76
monarch047 <monarch047@users.noreply.github.com>
2026-05-23 23:54:53 +0530

    Set standard User-Agent to bypass Cloudflare on VixCloud

--- src/en/streamingunity/src/eu/kanade/tachiyomi/animeextension/en/streamingunity/StreamingUnity.kt
+++ src/en/streamingunity/src/eu/kanade/tachiyomi/animeextension/en/streamingunity/StreamingUnity.kt
@@ -39,6 +39,7 @@ class StreamingUnity :
     private val injectApplication by injectLazy<Application>()
 
     override fun headersBuilder() = super.headersBuilder()
+        .set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
         .set("Referer", "$baseUrl/en/")
 
     // ============================== Popular ===============================
